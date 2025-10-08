bc3e2bca9ea9380f291f55fab70b57880985fd4a
Kauvinzera <kauvini2007@gmail.com>
2025-10-08 09:11:02 -0300

    Testes de soma e subtração

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,2 +1,11 @@
 def soma(a,b):
   return a+b
+
+def subtrair(a,b):
+  return a-b
+
+def multiplicacao(a,b):
+  return a*b
+
+def divisao(a,b):
+  return a/b
--- tests/test_calculadora.py
+++ tests/test_calculadora.py
@@ -1,4 +1,8 @@
 from src.calculadora import soma
+from src.calculadora import subtrair
 
 def test_soma():
   assert soma(2, 3) == 5
+
+def test_subtracao():
+  assert subtrair(10, 5) == 5
